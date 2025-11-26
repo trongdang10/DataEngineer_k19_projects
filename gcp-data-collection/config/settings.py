54a15fb44abb9329dd9e9b2973ce187f4b1bab15
trongdang10 <viettrong1010@gmail.com>
2025-11-26 12:29:22 +0100

    project template added

---
+++ gcp-data-collection/config/settings.py
@@ -0,0 +1,15 @@
+from dataclasses
+from pathlib import Path
+import os
+
+@dataclass
+class Settings:
+    gcp_project_id: str = os.environ["GCP_PROJECT_ID"]
+    gcp_region: str = os.environ["GCP_REGION"]
+    gcp_zone: str = os.environ["GCP_ZONE"]
+    gcs_raw_bucket: str = os.environ["GCS_RAW_BUCKET"]
+    service_account_key: Path = Path(os.environ["GCP_SERVICE_ACCOUNT_KEY"])
+    mongo_uri: str = os.environ["MONGO_URI"]
+    mongo_db: str = os.environ["MONGO_DB", "glamira"]
+
+settings = Settings()
